a876500b0f8de837590d733556b7729edb84bf79
fiyuang <fitrianggraini96@gmail.com>
2022-06-04 16:49:25 +0700

    state management with session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/fiyuang/golang-web-app/pkg/config"
 	"github.com/fiyuang/golang-web-app/pkg/handlers"
 	"github.com/fiyuang/golang-web-app/pkg/render"
@@ -12,9 +14,22 @@ import (
 
 const portNumber = ":9090"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
